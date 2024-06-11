77509b56bf50b29f4dc6ede4f348062c57f6bc06
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-06-11 17:02:14 +0300

    Remove comment controls, even if user is logged in

--- src/components/BlogComment.jsx
+++ src/components/BlogComment.jsx
@@ -1,48 +1,13 @@
 import { DateTime } from "luxon";
-import { useContext, useState } from "react";
-import useFetchWithAuth from "../api/fetch";
-import AuthContext from "../context/AuthContext";
-import CommentControls from "./CommentControls";
 import ContentDisplay from "./ContentDisplay";
 
-export default function BlogComment({ comment, onCommentUpdated }) {
-	const { user } = useContext(AuthContext);
-	const [newContent, setNewContent] = useState();
-	const fetch = useFetchWithAuth();
-
+export default function BlogComment({ comment }) {
 	const { user: commentUser, content, date: dateString } = comment;
 	const author = commentUser.username;
 	const date = new Date(dateString);
 	const dateFormatted = DateTime.fromJSDate(date).toLocaleString(
 		DateTime.DATETIME_MED
 	);
-
-	const onContentChanged = (content) => {
-		setNewContent(content);
-	};
-
-	const handleCommentUpdate = async () => {
-		const res = await fetch(
-			`/blog-posts/${comment.blogPost}/comments/${comment._id}`,
-			{
-				method: "PUT",
-				body: JSON.stringify({ content: newContent }),
-			},
-			true
-		);
-		onCommentUpdated();
-	};
-
-	const handleCommentDelete = async () => {
-		const res = await fetch(
-			`/blog-posts/${comment.blogPost}/comments/${comment._id}`,
-			{
-				method: "DELETE",
-			},
-			true
-		);
-		onCommentUpdated();
-	};
 
 	return (
 		<article className="blog-comment">
@@ -53,14 +18,6 @@ export default function BlogComment({ comment, onCommentUpdated }) {
 			<main className="comment-content">
 				<ContentDisplay htmlContent={content} />
 			</main>
-			{commentUser.username === user?.username ? (
-				<CommentControls
-					content={content}
-					onContentChanged={onContentChanged}
-					handleUpdate={handleCommentUpdate}
-					handleDelete={handleCommentDelete}
-				/>
-			) : null}
 		</article>
 	);
 }
